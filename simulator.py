0d659590ccc40f873e13b070f5c043cc85703785
Qiu Hai <qiuhaizzq@gmail.com>
2019-04-08 14:43:51 +0800

    SJF and SRTF

--- simulator.py
+++ simulator.py
@@ -11,15 +11,26 @@ Output files:
 '''
 import sys
 import copy
+import queue
 
 input_file = 'input.txt'
+def SRTF_lt(process_a, process_b):
+    return process_a.burst_time < process_b.burst_time or process_a.burst_time == process_b.burst_time and process_a.arrive_time < process_b.arrive_time
+
+def SJF_lt(process_a, process_b):
+    return process_a.pred_time < process_b.pred_time or process_a.pred_time == process_b.pred_time and process_a.arrive_time < process_b.arrive_time
 
 class Process:
     last_scheduled_time = 0
-    def __init__(self, id, arrive_time, burst_time):
+    def __init__(self, id, arrive_time, burst_time, lt=SRTF_lt):
         self.id = id
         self.arrive_time = arrive_time
         self.burst_time = burst_time
+        self.lt = lt
+        self.pred_time = 0
+
+    def __lt__(self, other):
+        return self.lt(self, other)
     #for printing purpose
     def __repr__(self):
         return ('[id %d : arrival_time %d,  burst_time %d]'%(self.id, self.arrive_time, self.burst_time))
@@ -50,45 +61,118 @@ def RR_scheduling(process_list_origin, time_quantum):
     process_index = 0
     process_list = copy.deepcopy(process_list_origin)
 
-    for process in process_list:
-        if process.id in process_pool:
-            process_pool[process.id].append(process)
-        else:
-            process_pool[process.id] = [process]
-            process_ids.append(process.id)
+    while process_ids or process_list:
+        while process_list and process_list[0].arrive_time<=current_time:
+            process = process_list.pop(0)
+            if process.id in process_pool:
+                process_pool[process.id].append(process)
+            else:
+                process_pool[process.id] = [process]
+            if process.id not in process_ids:
+                process_ids.append(process.id)
 
-    while len(process_ids)>0:
-        if(len(process_pool[process_ids[process_index]])>0):
-            current_process = process_pool[process_ids[process_index]][0]
+        if not process_ids:
+            current_time = process_list[0].arrive_time
+            continue
+        if process_index>=len(process_ids):
+            process_index = 0
+        current_pool = process_pool[process_ids[process_index]]
+        if(len(current_pool)>0):
+            current_process = current_pool[0]
             process_index = process_index+1
 
-            if (current_time < current_process.arrive_time):
-                current_time = current_process.arrive_time
-            schedule.append((current_time, current_process.id))
+            if(not schedule or current_process.id != schedule[-1][1]):
+                schedule.append((current_time, current_process.id))
             waiting_time = waiting_time + (current_time - current_process.arrive_time)
 
             if(current_process.burst_time<=time_quantum):
-                current_time = current_time + current_process.burst_time
+                current_time += current_process.burst_time
                 process_pool[current_process.id].pop(0)
             else:
-                current_time = current_time + time_quantum
+                current_time += time_quantum
                 current_process.burst_time-=time_quantum
+                current_process.arrive_time = current_time
         else:
             process_ids.pop(process_index)
-
-        if process_index>=len(process_ids):
-            process_index = 0
 
 
     average_waiting_time = waiting_time / float(len(process_list_origin))
     return schedule, average_waiting_time
     # return (["to be completed, scheduling process_list on round robin policy with time_quantum"], 0.0)
 
-def SRTF_scheduling(process_list):
-    return (["to be completed, scheduling process_list on SRTF, using process.burst_time to calculate the remaining time of the current process "], 0.0)
+def SRTF_scheduling(process_list_origin):
+    schedule = []
+    pqueue = queue.PriorityQueue()
+    current_time = 0
+    waiting_time = 0
+    process_list = copy.deepcopy(process_list_origin)
 
-def SJF_scheduling(process_list, alpha):
-    return (["to be completed, scheduling SJF without using information from process.burst_time"],0.0)
+    while not pqueue.empty() or process_list:
+        if pqueue.empty():
+            current_process = process_list.pop(0)
+            pqueue.put(current_process)
+            current_time = current_process.arrive_time
+
+        else:
+            current_process = pqueue.get()
+            if (not schedule or current_process.id != schedule[-1][1]):
+                schedule.append((current_time, current_process.id))
+            waiting_time = waiting_time + (current_time - current_process.arrive_time)
+
+            if process_list:
+                next_arrival_process = process_list[0]
+                if next_arrival_process.arrive_time <= current_process.arrive_time + current_process.burst_time:
+                    pqueue.put(process_list.pop(0))
+
+                    if(next_arrival_process.arrive_time < current_time + current_process.burst_time):
+                        current_process.burst_time -= next_arrival_process.arrive_time - current_time
+                        current_process.arrive_time = next_arrival_process.arrive_time
+                        pqueue.put(current_process)
+                    current_time = next_arrival_process.arrive_time
+                else:
+                    current_time += current_process.burst_time
+            else:
+                current_time +=current_process.burst_time
+
+
+    average_waiting_time = waiting_time / float(len(process_list_origin))
+    return schedule, average_waiting_time
+    # return (["to be completed, scheduling process_list on SRTF, using process.burst_time to calculate the remaining time of the current process "], 0.0)
+
+def SJF_scheduling(process_list_origin, alpha=0.5, init_time=5):
+    schedule = []
+    pqueue = queue.PriorityQueue()
+    current_time = 0
+    waiting_time = 0
+    process_list = copy.deepcopy(process_list_origin)
+    pred_history = {}
+
+    while not pqueue.empty() or process_list:
+        while (process_list and process_list[0].arrive_time <= current_time):
+            process = process_list.pop(0)
+            if process.id in pred_history:
+                new_pred = alpha*pred_history[process.id][1]+(1-alpha)*pred_history[process.id][0]
+                pred_history[process.id] = [new_pred,process.burst_time]
+            else:
+                pred_history[process.id] = [init_time, process.burst_time]
+
+            process.pred_time = pred_history[process.id][0]
+            process.lt = SJF_lt
+            pqueue.put(process)
+        if pqueue.empty():
+            current_time = process_list[0].arrive_time
+            continue
+
+        current_process = pqueue.get()
+        if (not schedule or current_process.id != schedule[-1][1]):
+            schedule.append((current_time, current_process.id))
+        waiting_time = waiting_time + (current_time - current_process.arrive_time)
+        current_time += current_process.burst_time
+
+    average_waiting_time = waiting_time / float(len(process_list_origin))
+    return schedule, average_waiting_time
+    # return (["to be completed, scheduling SJF without using information from process.burst_time"],0.0)
+
 
 
 def read_input():
